9f0f07fd0addd242754be58a2544128393531817
Robert <rquinones93@yahoo.com>
2019-05-31 16:34:15 -0700

    Finished Section 15, Programming To An Interface Example

---
+++ 15_ProgrammingToAnInterfaceExample/src/Computer.java
@@ -0,0 +1,46 @@
+// Interface we're programming to
+interface displayModule {
+  public void display ();
+}
+
+// Implements Interface
+class Monitor implements displayModule {
+  public void display () {
+    System.out.println( "Display through a monitor" );
+  }
+}
+
+// Implements Interface
+class Projector implements displayModule {
+  public void display () {
+    System.out.println( "Display through projector" );
+  }
+}
+
+// Class that includes objects we've created to
+// implement displayModule
+public class Computer {
+  displayModule displayModule;
+
+  public void setDisplayModule ( displayModule dm ) {
+    this.displayModule = dm;
+  }
+
+  public void display () {
+    this.displayModule.display();
+  }
+
+  public static void main(String args[]) {
+    Computer computer = new Computer();
+    // Client doesn't care about child class type
+    displayModule displayModuleOne = new Monitor();
+    displayModule displayModuleTwo = new Projector();
+
+    // Show different display types
+    computer.setDisplayModule( displayModuleOne );
+    computer.display();
+
+    computer.setDisplayModule( displayModuleTwo );
+    computer.display();
+  }
+}
