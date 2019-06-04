ef2a68ce895b6da9d79bf2d648109d0df8c5936e
Robert <rquinones93@yahoo.com>
2019-06-04 12:23:07 -0700

    Liskov Substitution Example

---
+++ LiskovSubstitutionExample/src/com/company/Vehicle.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public abstract class Vehicle {
+  abstract int getSpeed();
+  abstract int getCubicCapacity();
+}
+
+public class Car extends Vehicle {
+  int getSpeed() {}
+  int getCubicCapacity() {}
+  boolean sHatchBack() {}
+}
+
+public class Bus extends Vehicle {
+  int getSpeed() {}
+  int getCubicCapacity() {}
+  String getEmergenncyExitLocation() {}
+}
+
+  public static void main (String[] args ) {
+    Vehicle vehicle = new Bus();
+    vehicle.getSpeed();
+    vehicle = new Car();
+    vehicle.getCubicCapacity();
+  }
+}
